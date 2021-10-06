345ca39178713e02fbd82e71c68dfbb148c250ff
mona <mona.radad@gmail.com>
2021-10-06 14:18:56 -0700

    mypersonalwebsite commit

---
+++ Script.js
@@ -0,0 +1,10 @@
+var $ = document;
+
+function projectPage(){
+    window.location = "projects.html";
+}
+
+function linkedin(){
+    
+    var win = window.open("https://www.linkedin.com/in/mona-radad");
+}
